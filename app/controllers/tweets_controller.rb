6fcf723c359182114b423394e1dbbf44c4c9e941
Hadi Alhamed <hadialhamed.py@gmail.com>
2026-02-14 23:41:40 +0300

    handle realtime destroy and update

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -14,6 +14,12 @@ class TweetsController < ApplicationController
     end
   end
 
+  def destroy
+    @tweet = Tweet.find(params[:id])
+    @tweet.destroy
+    redirect_to tweets_path, notice: "Tweet was created successfully"
+  end
+
   private
   def tweet_params
     params.require(:tweet).permit(:body)
