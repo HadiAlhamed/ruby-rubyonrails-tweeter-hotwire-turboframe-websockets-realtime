0d9f2c272d2226f54a4042fe422419d3f9d65f24
Hadi Alhamed <hadialhamed.py@gmail.com>
2026-02-15 05:14:06 +0300

    add tweets controllers , every related portal should have same turbo_frame_tag

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,5 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only: [ :destroy, :update, :show, :edit ]
   def index
     @tweet = Tweet.new
     @tweets = Tweet.all.order(created_at: :desc)
@@ -6,22 +7,58 @@ class TweetsController < ApplicationController
 
   def create
     @tweet = Tweet.new(tweet_params)
-    if @tweet.save
-      redirect_to tweets_path, notice: "Tweet was created successfully"
-    else
-      @tweets = Tweet.all.order(created_at: :desc)
-      render :index, status: :unprocessable_entity
+
+    # 1. The Switchboard: Checks if the browser wants HTML or a Turbo Stream
+    respond_to do |format|
+      if @tweet.save
+        format.html { redirect_to tweets_path, notice: "Tweet was created successfully" }
+      else
+        @tweets = Tweet.all.order(created_at: :desc)
+
+        format.turbo_stream {
+          render turbo_stream: turbo_stream.replace("tweet_form",
+                partial: "tweets/form",
+                locals: { tweet: @tweet })
+        }
+        format.html { render :index, status: :unprocessable_entity }
+      end
     end
   end
 
   def destroy
-    @tweet = Tweet.find(params[:id])
     @tweet.destroy
-    redirect_to tweets_path, notice: "Tweet was created successfully"
+    respond_to do |format|
+        # This is for browsers with JS disabled or if you want a full reload
+        format.html { redirect_to tweets_path, notice: "Tweet deleted" }
+
+        # This tells the browser: "The model already sent the removal instruction,
+        # so just finish the request silently without a full page reload."
+        format.turbo_stream
+    end
   end
+
+
+  def edit
+  end
+  def show
+  end
+
+  def update
+    if @tweet.update(tweet_params)
+      # This redirect is caught by the Turbo Frame!
+      redirect_to @tweet, notice: "Tweet updated"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
 
   private
   def tweet_params
     params.require(:tweet).permit(:body)
+  end
+
+  def set_tweet
+        @tweet = Tweet.find(params[:id])
   end
 end
