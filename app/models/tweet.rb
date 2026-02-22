328450a7ebc292e984741179d61032ec41976a6e
Hadi Alhamed <hadialhamed.py@gmail.com>
2026-02-22 23:26:23 +0300

    bugfix : realtime and current_user_id bug : use refreshes instead of broadcast_to as a temp fix

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,5 +2,9 @@ class Tweet < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
   validates :body, presence: true, length: { minimum: 1, maximum: 280 }
-  broadcasts_to ->(tweet) { "tweets" }, inserts_by: :prepend
+  # after_create_commit { broadcast_prepend_to "tweets" }
+  # after_update_commit { broadcast_replace_to "tweets" }
+  # after_destroy_commit { broadcast_remove_to "tweets" }
+  # broadcasts_to ->(tweet) { "tweets" }, inserts_by: :prepend
+  broadcasts_refreshes_to ->(tweet) { "tweets" }
 end
